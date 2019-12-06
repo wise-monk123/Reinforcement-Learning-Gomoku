0c2e69dcec2a4fda178b7925899b524a85b72570
Laural1 <yingliuberkeley@gmail.com>
2019-12-06 14:48:11 -0800

    Update human_play.py

--- human_play.py
+++ human_play.py
@@ -44,14 +44,6 @@ def run():
     try:
         board = Board(width=width, height=height, n_in_row=n)
         game = Game(board)
-
-        # ############### human VS AI ###################
-        # load the trained policy_value_net in either Theano/Lasagne, PyTorch or TensorFlow
-
-        # best_policy = PolicyValueNet(width, height, model_file = model_file)
-        # mcts_player = MCTSPlayer(best_policy.policy_value_fn, c_puct=5, n_playout=400)
-
-        # load the provided model (trained in Theano/Lasagne) into a MCTS player written in pure numpy
         try:
             policy_param = pickle.load(open(model_file, 'rb'))
         except:
@@ -62,7 +54,7 @@ def run():
                                  c_puct=5,
                                  n_playout=400)  # set larger n_playout for better performance
 
-        # uncomment the following line to play with pure MCTS (it's much weaker even with a larger n_playout)
+        # uncomment the following line to play with pure MCTS 
         # mcts_player = MCTS_Pure(c_puct=5, n_playout=1000)
 
         # human player, input your move in the format: 2,3
